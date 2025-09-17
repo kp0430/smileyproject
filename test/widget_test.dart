94f90d496acc9ef9977231dde03c81cf24b83c53
kp0430 <122626179+kp0430@users.noreply.github.com>
2025-09-16 22:14:19 -0400

    Remove sample code, add head and lips
    Remove the sample code and add a yellow circle for the head to look like an emoji. Also add a dimple for some cheekiness

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -13,7 +13,7 @@ import 'package:smileyproject/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const ShapesDemoApp());
+    await tester.pumpWidget(const SmileyApp());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
